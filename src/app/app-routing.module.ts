a8b27a3344d8a0cdf16e1246062469c36094cec4
brenocruvinel <breno.cruvinel@hotmail.com>
2021-02-14 12:13:54 -0300

    commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'usuarios',
+		loadChildren: () => import('./users/users.module').then(m => m.UsersModule),
+  },
+  {
+    path: '',
+		component: DashboardComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/menu-left/menu-left.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core'
+
+@Component({
+    selector: 'app-menu-left',
+    templateUrl: './menu-left.component.html'
+})
+export class MenuLeftComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit(): void { }
+}
